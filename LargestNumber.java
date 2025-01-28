adf3383e8f7bf9e964a9b4c8e37d5914c4212592
Poojitha1616 <gudemsaipoojitha2@gmail.com>
2025-01-28 23:00:16 +0530

    Add files via upload
    This program displays the largest number.

---
+++ LargestNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        if (num1 >= num2) {
+            if (num1 >= num3) {
+                System.out.println(num1 + " is the largest number.");
+            } else {
+                System.out.println(num3 + " is the largest number.");
+            }
+        } else {
+            if (num2 >= num3) {
+                System.out.println(num2 + " is the largest number.");
+            } else {
+                System.out.println(num3 + " is the largest number.");
+            }
+        }
+    }
+}
+
